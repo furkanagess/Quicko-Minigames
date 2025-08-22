e70548157c2fcd2545671cf01d52311b9093ed86
Furkan Caglar <caglarrfurkann@gmail.com>
2025-08-22 23:43:29 +0300

    Implement centralized AppBar system and update project dependencies
    - Introduced a centralized AppBar management system to enhance performance and maintain consistency across the app, reducing code duplication by approximately 70%.
    - Updated various screens to utilize the new AppBar implementations, ensuring a unified user experience.
    - Added new dependencies for cloud_firestore and connectivity_plus to support enhanced features.
    - Removed outdated sound assets and related documentation to streamline project structure.
    - Updated pubspec.yaml and pubspec.lock to reflect changes in dependencies and versioning.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,8 @@ import FlutterMacOS
 import Foundation
 
 import audioplayers_darwin
+import cloud_firestore
+import connectivity_plus
 import firebase_analytics
 import firebase_core
 import firebase_crashlytics
@@ -18,6 +20,8 @@ import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   FirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FirebaseAnalyticsPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -4,6 +4,8 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   audioplayers_windows
+  cloud_firestore
+  connectivity_plus
   firebase_core
   share_plus
   url_launcher_windows
